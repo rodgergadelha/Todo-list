34fe0b9d06b7eb41442364093f186a0f3a019215
Rodger <iamapicle64@gmail.com>
2023-03-24 13:44:34 -0300

    final update

--- src/app.js
+++ src/app.js
@@ -1,53 +1,49 @@
+import {createApp} from 'vue'
 import Todos from './api/todos';
 import './assets/css/main.css';
 
 const apiTodos = new Todos();
 
-async function exec() {
-    const todos = await apiTodos.index();
+const app = createApp({
+    data() {
+        return {
+            todos: [],
+            form: {
+                text: "",
+                done: false
+            }
+        }
+    },
+    async created() {
+        this.fetchTodos();
+    },
+    methods: {
+        async fetchTodos() {
+            this.todos = await apiTodos.index();
+            console.log(this.todos)
+        },
+        async createTodo() {
+            const data = await apiTodos.store(this.form);
+            this.todos.push(data);
+            this.form.text = "";
+            this.form.done = false;
+        },
+        async toggleTodoStatus(todo) {
+            const data = await apiTodos.update({
+                ...todo,
+                done: !todo.done
+            });
 
-    const ul = document.querySelector("ul.todos");
-    
-    todos.forEach(todo => {
-        
-        // todo li
-        const li = document.createElement("li");
-        li.setAttribute("class", "todo");
+            const index = this.todos.findIndex(({id}) => id === data.id);
+            this.todos[index] = data;
+        },
+        async destroyTodo(id) {
+            await apiTodos.destroy({id});
 
-        //todo checkbox
-        const todoCheckbox = document.createElement("label");
-        todoCheckbox.setAttribute("class", "checkbox-input");
+            const index = this.todos.findIndex((todo) => todo.id === id);
+            this.todos.splice(index, 1);
+        }
+    }
+});
 
-        // todo checkbox -> input
-        const inputCheckbox = document.createElement("input");
-        inputCheckbox.setAttribute("type", "checkbox");
-        if(todo.done) inputCheckbox.setAttribute("checked", "checked");
-
-        // todo checkbox span
-        const spanCheckbox = document.createElement("span");
-        spanCheckbox.setAttribute("class", "checkmark");
-        
-        todoCheckbox.appendChild(inputCheckbox);
-        todoCheckbox.appendChild(spanCheckbox);
-
-        // todo text
-        const todoText = document.createElement("span");
-        todoText.setAttribute("class", "todo-text");
-
-        const spanText = document.createTextNode(todo.text);
-        todoText.appendChild(spanText);
-
-        // todo delete
-        const todoDelete = document.createElement("span");
-        todoDelete.setAttribute("class", "todo-delete");
-
-        li.appendChild(todoCheckbox);
-        li.appendChild(todoText);
-        li.appendChild(todoDelete);
-
-        ul.appendChild(li);
-
-    });
-}
-
-exec();
+app.mount("#app");
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
     entry: ['@babel/polyfill', path.resolve(__dirname, './src/app.js')],
@@ -16,5 +17,17 @@ module.exports = {
             use: ["style-loader", "css-loader"],
           },
         ],
-    }
+    },
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.esm-bundler.js',
+      },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+    ],
+    
 }
