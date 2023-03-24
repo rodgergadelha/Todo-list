17a05bfa4f099c3d396b76942092e5291ffe0ff6
Rodger <iamapicle64@gmail.com>
2023-03-24 10:49:10 -0300

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,53 @@
 import Todos from './api/todos';
 import './assets/css/main.css';
 
+const apiTodos = new Todos();
+
+async function exec() {
+    const todos = await apiTodos.index();
+
+    const ul = document.querySelector("ul.todos");
+    
+    todos.forEach(todo => {
+        
+        // todo li
+        const li = document.createElement("li");
+        li.setAttribute("class", "todo");
+
+        //todo checkbox
+        const todoCheckbox = document.createElement("label");
+        todoCheckbox.setAttribute("class", "checkbox-input");
+
+        // todo checkbox -> input
+        const inputCheckbox = document.createElement("input");
+        inputCheckbox.setAttribute("type", "checkbox");
+        if(todo.done) inputCheckbox.setAttribute("checked", "checked");
+
+        // todo checkbox span
+        const spanCheckbox = document.createElement("span");
+        spanCheckbox.setAttribute("class", "checkmark");
+        
+        todoCheckbox.appendChild(inputCheckbox);
+        todoCheckbox.appendChild(spanCheckbox);
+
+        // todo text
+        const todoText = document.createElement("span");
+        todoText.setAttribute("class", "todo-text");
+
+        const spanText = document.createTextNode(todo.text);
+        todoText.appendChild(spanText);
+
+        // todo delete
+        const todoDelete = document.createElement("span");
+        todoDelete.setAttribute("class", "todo-delete");
+
+        li.appendChild(todoCheckbox);
+        li.appendChild(todoText);
+        li.appendChild(todoDelete);
+
+        ul.appendChild(li);
+
+    });
+}
+
+exec();
